b20f2b6ff781a953c2bf8d8fc7f3e93732aecf94
Abdul Haseeb Ahmad Bashir <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2026-06-22 22:17:23 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.go
@@ -0,0 +1,15 @@
+
+func isPalindrome(s string) bool {
+
+	isPalindrome := true	
+	re := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	noSpaceString :=  re.ReplaceAllString(strings.ReplaceAll(strings.ToLower(s)," ",""), "")
+	lastIndex := len(noSpaceString) - 1
+	for i := 0;  i < lastIndex  ; i++ {
+			if rune(noSpaceString[i]) !=  rune(noSpaceString[lastIndex]){
+				isPalindrome = false
+			} 
+			lastIndex = lastIndex - 1
+	}
+	return isPalindrome
+}
