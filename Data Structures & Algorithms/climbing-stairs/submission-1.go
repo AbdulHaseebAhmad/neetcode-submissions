71f0d14f3c90de713cfa0c00938048fb200a1cac
Abdul Haseeb Ahmad Bashir <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2026-07-03 06:37:42 +0100

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.go
@@ -0,0 +1,16 @@
+func climbStairs(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+
+	prev2 := 1  
+	prev1 := 1  
+
+	for i := 2; i <= n; i++ {
+		current := prev1 + prev2
+		prev2 = prev1
+		prev1 = current
+	}
+
+	return prev1
+}
